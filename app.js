63a399e0eb2a38a9e4f3f78922d37545eadd3914
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-12-02 11:35:22 -0500

    exercise 11

--- app.js
+++ app.js
@@ -127,5 +127,46 @@ function catSent(noun = 'cat', adjective = 'white') {
 // console.log(pizza)
 
 // Exercise 10: Boolean gates
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = 'fr';  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+
+let LANG = localLangConfig
+
+// Log the result
+console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = 'dark'; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+const USER_THEME = userSavedTheme
+
+// Log the result
+console.log('User theme setting:', USER_THEME);
 
 
+// Exercise 11: Optional chaining
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+    name: 'Alice',
+  };
+  
+  let cat = adventurer.age?.name; // Your code here
+  
+  console.log(cat);
+  
